9d6e87a5d5f429fe6956b4257b350ec903d0c26e
Bruno Martins <bgcngm@gmail.com>
2018-08-16 15:19:28 +0100

    raphael: Switch to common basic USB HAL
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab

--- device.mk
+++ device.mk
@@ -405,7 +405,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service.basic
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
