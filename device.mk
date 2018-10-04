9ffe8ca423bdc2b93622fae296ab84f6fc373a22
TheScarastic <warabhishek@gmail.com>
2018-10-04 06:53:00 -0400

    raphael: Build tinymix
    Change-Id: If94141b0678817e7969e2edaf9ce1e6c4e1de006
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- device.mk
+++ device.mk
@@ -111,7 +111,8 @@ PRODUCT_PACKAGES += \
     libsndmonitor \
     libtinycompress \
     libvisualizer \
-    libvolumelistener
+    libvolumelistener \
+    tinymix
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
