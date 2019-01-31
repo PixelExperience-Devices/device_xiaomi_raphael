e4a2ac64e55abfc5bed7eac2c5d32ccdbbbc2206
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-01-31 18:20:50 -0200

    raphael: Build thermal controller

--- device.mk
+++ device.mk
@@ -143,6 +143,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
+# ThermalController app
+PRODUCT_PACKAGES += \
+    ThermalController
+
 # VNDK-SP
 PRODUCT_PACKAGES += \
     vndk_package
