90d6ac0bc437d9d3c52dee0fa1258e26cf5acd7c
Peter Cai <peter@typeblog.net>
2019-07-05 07:40:23 +0000

    raphael: Enable and configure FOD HAL

--- device.mk
+++ device.mk
@@ -54,6 +54,10 @@ PRODUCT_PACKAGES += \
     libvulkan \
     vendor.display.config@1.0
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_raphael
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
