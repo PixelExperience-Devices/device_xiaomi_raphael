24a5549c2e029012c031ea5f97c2eb47d125c31d
UtsavisGreat <utsavbalar1231@gmail.com>
2019-08-17 16:12:33 +0000

    raphael: fod: use fingerprint icon location & size values from stock

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -34,9 +34,9 @@
 #define FOD_STATUS_ON 1
 #define FOD_STATUS_OFF 0
 
-#define FOD_SENSOR_X 455
-#define FOD_SENSOR_Y 1920
-#define FOD_SENSOR_SIZE 173
+#define FOD_SENSOR_X 445
+#define FOD_SENSOR_Y 1931
+#define FOD_SENSOR_SIZE 190
 
 namespace {
 
