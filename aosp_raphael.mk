d2192248653ecbfa432c362a145b3373bef717be
UtsavisGreat <utsavbalar1231@gmail.com>
2019-08-22 05:01:50 +0000

    raphael: use walleye april build fingerprint
    to satisfy SafetyNet Check

--- aosp_raphael.mk
+++ aosp_raphael.mk
@@ -18,7 +18,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi K20 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/raphael/raphael:9/PKQ1.181121.001/V10.3.12.0.PFKCNXM:user/release-keys"
+BUILD_FINGERPRINT := "google/walleye/walleye:9/PQ2A.190405.003/5310204:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="raphael-user 9 PKQ1.181121.001 V10.3.12.0.PFKCNXM release-keys" \
