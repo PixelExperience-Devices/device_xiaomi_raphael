556da8fd30880ac27888670f207abc208df24b66
TheScarastic <warabhishek@gmail.com>
2019-09-06 13:55:19 +0530

    raphael: Import stock audio_platform_info

--- device.mk
+++ device.mk
@@ -33,6 +33,7 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
 
 # Camera
