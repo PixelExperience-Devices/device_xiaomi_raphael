0cc56514a09a6a7d7092d4c110eb47894a7ab1b6
TheScarastic <warabhishek@gmail.com>
2019-10-04 12:24:06 +0530

    raphael: Adjust qcom sepolicy path for new repo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,10 +137,14 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # Sepolicy
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    $(DEVICE_PATH)/sepolicy/private \
+    device/qcom/sepolicy/generic/private \
+    device/qcom/sepolicy/qva/private
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/qcom/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/qcom/sepolicy/public
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/qcom/sepolicy/generic/public \
+    device/qcom/sepolicy/qva/public
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true
