8e4855928fb744b0c6d9f8da6422ad1d0baadbb0
TheScarastic <warabhishek@gmail.com>
2019-10-05 12:42:58 +0530

    raphael: Cleanup powerhal makefile

--- power/Android.mk
+++ power/Android.mk
@@ -3,14 +3,35 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libbase libhidlbase libhidltransport libutils android.hardware.power@1.2
-LOCAL_HEADER_LIBRARIES += libutils_headers
-LOCAL_HEADER_LIBRARIES += libhardware_headers
-LOCAL_SRC_FILES := power-common.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c service.cpp Power.cpp
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libcutils \
+    libdl \
+    libxml2 \
+    libbase \
+    libhidlbase \
+    libhidltransport \
+    libutils \
+    android.hardware.power@1.2
+
+LOCAL_HEADER_LIBRARIES += \
+    libutils_headers \
+    libhardware_headers
+
+LOCAL_SRC_FILES := \
+    power-common.c \
+    metadata-parser.c \
+    utils.c list.c \
+    hint-data.c \
+    powerhintparser.c \
+    service.cpp \
+    Power.cpp \
+    power-msmnile.c
+
 LOCAL_C_INCLUDES := external/libxml2/include \
                     external/icu/icu4c/source/common
 
-LOCAL_SRC_FILES += power-msmnile.c
+LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
 
 ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
     LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
@@ -23,6 +44,4 @@ endif
 LOCAL_MODULE := android.hardware.power@1.2-service
 LOCAL_INIT_RC := android.hardware.power@1.2-service.rc
 LOCAL_MODULE_TAGS := optional
-LOCAL_HEADER_LIBRARIES := generated_kernel_headers
-LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
 include $(BUILD_EXECUTABLE)
