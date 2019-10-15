430f4338e01c1fa62b047e7acb8c4b6b7ec44ea4
Patrick Tjin <pattjin@google.com>
2019-10-15 10:32:37 +0800

    raphael: Add nosdcard characteristic to device
    * Just as per stock.
    
    Bug: 30895189
    Change-Id: Ic2e040dc8d811ca84c910d42f6f7db35526802ff
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- device.mk
+++ device.mk
@@ -9,6 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
+PRODUCT_CHARACTERISTICS := nosdcard
+
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/raphael/raphael-vendor.mk)
 
