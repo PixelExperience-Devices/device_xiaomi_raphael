9051d8e8da7bbcc3e31c8ef496ca55cf632b7f09
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-11-25 01:31:44 +0000

    raphael: Don't use indian fingerprint as default
    * We're overriding it on init so there's no reason to keep one by default

--- aosp_raphael.mk
+++ aosp_raphael.mk
@@ -18,6 +18,4 @@ PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_NAME := aosp_raphael
 
-BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
