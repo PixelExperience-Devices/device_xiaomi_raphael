7225efff15203048c5e12a59e89c97cd9782489a
Mahesh Sharma <smahesh@codeaurora.org>
2019-12-13 14:35:14 -0800

    raphael: ANT: Support new ANT stack
    Add appropriate package name on the basis of
    transport used on target.
    
    Change-Id: I02163a7a50afba8c6ee6a766327b78c5df081d4e
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- device.mk
+++ device.mk
@@ -78,8 +78,9 @@ PRODUCT_COPY_FILES += \
 
 # ANT+
 PRODUCT_PACKAGES += \
-    AntHalService \
-    antradio_app
+    AntHalService-Soong \
+    com.dsi.ant.antradio_library \
+    com.dsi.ant@1.0
 
 # Atrace
 PRODUCT_PACKAGES += \
