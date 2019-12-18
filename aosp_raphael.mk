28c40bc409dae53f7760938b1df0b9d75b4f318e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-12-18 15:33:18 +0000

    raphael: Track and include customized version of ANX Camera

--- aosp_raphael.mk
+++ aosp_raphael.mk
@@ -19,3 +19,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_NAME := aosp_raphael
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Inherit from custom vendor
+$(call inherit-product, vendor/MiuiCamera/config.mk)
