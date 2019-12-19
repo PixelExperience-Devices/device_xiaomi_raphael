fefbe0466dccf54861bd44f67420ac7a269c4d57
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-12-19 21:33:11 +0000

    raphael: Disable google recorder for now
    Also update gapps flags

--- aosp_raphael.mk
+++ aosp_raphael.mk
@@ -16,10 +16,11 @@
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_raphael
