b8fd5975c7558035a28f79c899b055014b9f108a
Nauval Rizky <enuma.alrizky@gmail.com>
2020-01-02 01:40:28 +0900

    raphael: overlay the init.qcom.usb.sh
    * For modification later that will fix weird
      usb debugging that keep resetting after reboot
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -15,3 +15,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.usb.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.usb.sh
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
+include $(BUILD_PREBUILT)
