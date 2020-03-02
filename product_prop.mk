545bd837722d60c02e6f793f3427e5db541c9564
Peter Cai <peter@typeblog.net>
2020-03-02 16:49:27 +0800

    raphael: Override ro.adb.secure for eng builds
    * `/vendor/default.prop` is setting `ro.adb.secure=1`. Previously I did
      a dirty hack in adbd, but apparently this is the better solution.
    
    Change-Id: Ib72e3e96d36b69b74da9bbb8b15246d1dde5ad84

--- product_prop.mk
+++ product_prop.mk
@@ -4,6 +4,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Adb
+ifeq ($(TARGET_BUILD_VARIANT),eng)
+# /vendor/default.prop is force-setting ro.adb.secure=1
+# Get rid of that by overriding it in /product on eng builds
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.secure=0 \
+    ro.adb.secure=0
+endif
+
 # Audio
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.bluetooth.a2dp_offload.supported=false \
