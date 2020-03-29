aaf70894a036aa7152bf53e38658a0e565907f67
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-29 17:17:44 -0300

    raphael: parts: Let system start up popup camera service

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -24,7 +24,6 @@ import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
-import org.lineageos.settings.popupcamera.PopupCameraUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -36,6 +35,5 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DiracUtils.initialize(context);
         DozeUtils.checkDozeService(context);
-        PopupCameraUtils.startService(context);
     }
 }
