30cea60ccd02bcf707ff21e8420fa848c1103634
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-04-04 10:31:01 -0300

    raphael: Cleanup overlays

--- device.mk
+++ device.mk
@@ -38,8 +38,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pe
+    $(LOCAL_PATH)/overlay
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
