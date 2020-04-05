23653d7e4a80679c768d965ff8e1876229073c73
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-04-05 15:47:01 -0300

    raphael: parts: PopupCameraService: take back motor when shutdown
    * Do the same on boot completed

--- parts/src/org/lineageos/settings/popupcamera/PopupCameraService.java
+++ parts/src/org/lineageos/settings/popupcamera/PopupCameraService.java
@@ -37,6 +37,7 @@ import android.net.Uri;
 import android.os.CountDownTimer;
 import android.os.IBinder;
 import android.os.Handler;
+import android.os.RemoteException;
 import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.UserHandle;
@@ -267,6 +268,18 @@ public class PopupCameraService extends Service {
         }
     }
 
+    private void onBootCompleted(){
+        try {
+            int status = mMotor.getMotorStatus();
+            if (status == MOTOR_STATUS_POPUP_OK ||
+                    status == MOTOR_STATUS_TAKEBACK_JAMMED) {
+                mCameraState = closeCameraState;
+                mMotor.takebackMotor(1);
+            }
+        } catch(RemoteException e) {
+        }
+    }
+
     private void forceTakeback(){
         mCameraState = closeCameraState;
         updateMotor();
@@ -274,8 +287,10 @@ public class PopupCameraService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand");
         if (DEBUG) Log.d(TAG, "Starting service");
         setProximitySensor(true);
+        onBootCompleted();
         return START_STICKY;
     }
 
@@ -294,6 +309,7 @@ public class PopupCameraService extends Service {
 
     private void registerReceiver() {
         IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SHUTDOWN);
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_CAMERA_STATUS_CHANGED);
         registerReceiver(mIntentReceiver, filter);
@@ -309,7 +325,7 @@ public class PopupCameraService extends Service {
                    dismissProximityObstructedDialog();
                }
                updateMotor();
-            }else if (Intent.ACTION_SCREEN_OFF.equals(action)) {
+            }else if (Intent.ACTION_SCREEN_OFF.equals(action) || Intent.ACTION_SHUTDOWN.equals(action)) {
                 if (mCameraState.equals(openCameraState)){
                     forceTakeback();
                 }
