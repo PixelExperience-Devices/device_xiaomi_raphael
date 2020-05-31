4e2edf476250a66394afeb6024441a8236a0f560
TheScarastic <warabhishek@gmail.com>
2020-05-31 14:36:09 +0000

    raphael: switch to qcom bt stack

--- system_prop.mk
+++ system_prop.mk
@@ -1,7 +1,11 @@
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.bt.a2dp.aac_whitelist=false \
     persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aptxadaptive-aac-ldac \
     persist.vendor.btstack.enable.splita2dp=true \
+    persist.vendor.btstack.enable.twsplus=true \
+    persist.vendor.btstack.enable.twsplussho=true \
+    ro.bluetooth.library_name=libbluetooth_qti.so \
     vendor.bluetooth.soc=cherokee
 
 # Camera
