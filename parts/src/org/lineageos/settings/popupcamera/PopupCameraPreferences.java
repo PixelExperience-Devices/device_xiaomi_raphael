8d7f92e6d6e63a264d9db68a6797dd2ef3e70fcf
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-06-04 19:44:42 -0300

    raphael: Fix settings resetting after reboot
    When using getDefaultSharedPreferences before unlocking and directBootAware enabled, settings gets reset. Fix this behavior by adding proper check

--- parts/src/org/lineageos/settings/popupcamera/PopupCameraPreferences.java
+++ parts/src/org/lineageos/settings/popupcamera/PopupCameraPreferences.java
@@ -18,12 +18,14 @@ package org.lineageos.settings.popupcamera;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.UserManager;
 import android.preference.PreferenceManager;
 
 public class PopupCameraPreferences {
 
     private static final String TAG = "PopupCameraUtils";
     private static final boolean DEBUG = false;
+    private Context mContext;
     private SharedPreferences mSharedPrefs;
 
     private static final String LED_EFFECT_KEY = "popup_led_effect";
@@ -33,14 +35,32 @@ public class PopupCameraPreferences {
     private static final String SOUND_EFFECT_DEFAULT_VALUE = "0";
 
     public PopupCameraPreferences(Context context) {
-        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        mContext = context;
+        ensureSharedPrefs();
+    }
+
+    private void ensureSharedPrefs(){
+        UserManager um = mContext.getSystemService(UserManager.class);
+        if (mContext.isCredentialProtectedStorage() && !um.isUserUnlocked()) {
+            mSharedPrefs = null;
+        }else{
+            mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+        }
     }
 
     public String getSoundEffect() {
+        ensureSharedPrefs();
+        if (mSharedPrefs == null){
+            return SOUND_EFFECT_DEFAULT_VALUE;
+        }
         return mSharedPrefs.getString(SOUND_EFFECT_KEY, SOUND_EFFECT_DEFAULT_VALUE);
     }
 
     public boolean isLedAllowed() {
+        ensureSharedPrefs();
+        if (mSharedPrefs == null){
+            return LED_EFFECT_DEFAULT_VALUE;
+        }
         return mSharedPrefs.getBoolean(LED_EFFECT_KEY, LED_EFFECT_DEFAULT_VALUE);
     }
 }
