5d94a76c941b2507d7bd2e7cdcdb2a1d5b5dde62
Jesse Chan <jc@lineageos.org>
2020-06-13 22:45:25 +0800

    raphael: enable unspecialized app process pool
    Change-Id: I6dfcb59dace5eaa7551caeae86db3b485c0cfbf1

--- system_prop.mk
+++ system_prop.mk
@@ -103,3 +103,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Vendor
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.va_aosp.support=1
+
+# Zygote
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.device_config.runtime_native.usap_pool_enabled=true
