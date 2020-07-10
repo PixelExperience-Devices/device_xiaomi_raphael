82731242bca8e8e5f32cd9088ec689cb5de070e1
Nolen Johnson <johnsonnolen@gmail.com>
2020-07-10 12:10:21 -0400

    raphael: bluetooth: Disable clean turn on
    * This isn't needed anymore.
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: Ib77e5212099d82bbc07aea6044eb35109faff5cb

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -67,8 +67,6 @@ static inline const char* BtmGetDefaultName() {
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
-// Skips conn update at conn completion
-#define BT_CLEAN_TURN_ON_DISABLED 1
 // Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec
 #define AVDT_NUM_SEPS 12
 
