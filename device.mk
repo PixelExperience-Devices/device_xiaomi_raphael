b21a1e720fd6872daecf2c3a4d277465913fe298
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-07-24 15:22:50 -0300

    Revert "raphael: override overlays present in stock vendor"
    This reverts commit c46e4d5be91e9c7cf0584649173d79804272ac43.

--- device.mk
+++ device.mk
@@ -140,13 +140,6 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     vendor/nxp/opensource/sn100x
 
-# Overlays - override vendor ones
-PRODUCT_PACKAGES += \
-    FrameworksResCommon \
-    FrameworksResTarget \
-    DevicesOverlay \
-    DevicesAndroidOverlay
-
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
