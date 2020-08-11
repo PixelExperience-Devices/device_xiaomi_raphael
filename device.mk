d3f64e7a643d1fb801412e11190fca8fb3319947
kazerine <kamikaze.mrinal@gmail.com>
2020-08-12 00:18:36 +0530

    raphael: Get rid of megvii dependencies in camera.qcom.so
    Change-Id: I97058c320d06819a468ebe38e2704360e9aa91db
    Signed-off-by: Azure-Helper <azurehelper219@gmail.com>

--- device.mk
+++ device.mk
@@ -134,6 +134,7 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
     libgui_vendor \
+    libshim_megvii \
     libstdc++.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor \
