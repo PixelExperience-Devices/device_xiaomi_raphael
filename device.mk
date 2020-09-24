aa027acf4a1a5a2afb18d08398928881001c6e1e
Haoran.Wang <elven.wang@nxp.com>
2020-09-24 22:20:16 +0800

    raphael: Grant android.permission.WRITE_DEVICE_CONFIG to gms
    Grant android.permission.WRITE_DEVICE_CONFIG to com.google.android.gms
    package.
    
    TEST: run gts -m GtsDeviceConfigTestCases -t
    com.google.android.deviceconfig.gts.DeviceConfigApiPermissionTests#testWriteDeviceConfigPermission
    
    Signed-off-by: Haoran.Wang <elven.wang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/13666
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    Reviewed-by: Tian Yang <yang.tian@nxp.com>

--- device.mk
+++ device.mk
@@ -228,6 +228,10 @@ $(call inherit-product-if-exists, vendor/GoogleCamera/GoogleCamera.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/pixel/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
 
+# GMS Permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-gms.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp.permissions-gms.xml
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.1.vendor \
