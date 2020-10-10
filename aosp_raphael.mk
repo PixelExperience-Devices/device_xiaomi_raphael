52be562c2041fd4f69ae560d7916bff5fb4e8681
Jorge Lucas <jorgelucas@pixelexperience.org>
2020-10-10 10:55:49 -0300

    Revert "raphael: Enable google recorder"
    This reverts commit a5b2d0fafb85d73ed4741004d29e20850f2201b7.

--- aosp_raphael.mk
+++ aosp_raphael.mk
@@ -10,7 +10,6 @@ $(call inherit-product, device/xiaomi/raphael/device.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
