fbf3744243062bba408e2b3779b44677a370084b
Adhitya Mohan <me@adhityamohan.in>
2020-11-15 15:48:46 +0530

    raphael: Update DRM HAL entries
    * Remove deprecated drm 1.0
    * push clearkey and widevine into seprate fragment files
    * import widevine manifest file from sunfish
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>

--- device.mk
+++ device.mk
@@ -171,10 +171,8 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.4.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.4.vendor
 
 # Fingerprint
 PRODUCT_PACKAGES += \
