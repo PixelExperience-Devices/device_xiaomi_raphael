ac4ac87738abd280be5a2f804a4b69798b21cec7
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-12-01 19:39:42 +0530

    raphael: Bind mount Wifi Aware feature permissions.
    - Fixes issues when GPS and Wifi are switched on simultaneously.
    - Also kills logspam regarding NAN.
    
    Change-Id: Id56b2bbc720bf2de96aad825956bc63db3d4e694
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- device.mk
+++ device.mk
@@ -238,6 +238,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
+    init.raphael.wlan.rc \
     init.recovery.qcom.rc \
     init.target.rc \
     init.xiaomi.rc \
