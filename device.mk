6ba1aaed4b165bdfdf9728ce3b128cb73d7c334a
PIPIPIG233666 <pig.priv@gmail.com>
2020-12-12 18:28:08 -0500

    raphael: Import Xiaomi changes over USB scripts
    Change-Id: I0474d8faf5ae170be77ebfd4438f0ae913234c8d

--- device.mk
+++ device.mk
@@ -238,6 +238,7 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
     init.qti.chg_policy.sh \
+    init.mi.usb.sh \
     init.recovery.qcom.rc \
     init.target.rc \
     init.xiaomi.rc \
