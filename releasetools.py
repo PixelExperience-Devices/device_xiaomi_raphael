5c2b3eb74f5c8f8a25a4518328d1afde0b9ebf53
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-02-16 14:39:25 +0530

    raphael: releasetools: Print a better message while flashing
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- releasetools.py
+++ releasetools.py
@@ -30,7 +30,7 @@ def AddImage(info, basename, dest):
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
 def OTA_InstallEnd(info):
-  info.script.Print("Patching firmware images...")
+  info.script.Print("Patching dtbo and vbmeta images...")
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   return
