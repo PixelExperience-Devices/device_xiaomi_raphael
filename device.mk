b9deb32d478eee1d511d9bdb03a26b1cdc802192
naveenjohnsonv <14140949+naveenjohnsonv@users.noreply.github.com>
2021-03-31 11:58:06 +0530

    raphael: remove MusicFX for Dolby

--- device.mk
+++ device.mk
@@ -415,6 +415,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RIL
 PRODUCT_PACKAGES += \
     CarrierConfigOverlay \
