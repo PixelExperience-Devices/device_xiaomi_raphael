46f30331d56ae1c17d3276f14f444d3ed9e1b4c4
Sonal Singh <sonal.singh.19993@gmail.com>
2021-04-07 17:09:51 +0530

    raphael: Build libminijail and libavservices_minijail from source
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- device.mk
+++ device.mk
@@ -282,6 +282,12 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Minijail
+PRODUCT_PACKAGES += \
+    libminijail \
+    libavservices_minijail \
+    libavservices_minijail.vendor
+
 # Native Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
