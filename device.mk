8d39cd98f97c3524429235bd4427658fcbadaa2c
Chirayu Desai <chirayudesai1@gmail.com>
2021-04-15 02:09:34 +0530

    raphael: rro_overlays: Move CarrierConfig to RRO
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: Ia57acd244608848f70fdcc30d5694c919db60671

--- device.mk
+++ device.mk
@@ -400,6 +400,7 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    CarrierConfigOverlay \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
