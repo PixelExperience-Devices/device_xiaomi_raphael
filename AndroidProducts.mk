91ae2c010e621824fdd54d290795cc14b2eab906
TH779 <i@779.moe>
2021-06-01 16:01:21 +0800

    raphael: Initial skeleton tree
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I21b59a6c243d9466c94b8193138b6ab53aa6cbc2

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2021 The PixelExperience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_raphael.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_raphael-userdebug \
+    aosp_raphael-eng
---
+++ device.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2021 The PixelExperience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
