9a97d4aa8510c96e05d53b23c3dadc7e15d6ad7c
TH779 <i@779.moe>
2021-06-02 01:12:08 +0800

    raphael: Import AID/fs config
     * From LA.UM.9.1.r1-09600-SMxxx0.0.
    
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: Idfbd73b504c231cbd84bd6dbc2059cd6189717a0

--- device.mk
+++ device.mk
@@ -14,6 +14,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# AID/fs configs
+PRODUCT_PACKAGES += \
+    fs_config_files
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
