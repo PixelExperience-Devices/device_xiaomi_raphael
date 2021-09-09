e5601b799505b1790998ce55d9dac8ee5e34502f
TH779 <i@779.moe>
2021-09-09 22:16:04 +0800

    Revert "raphael: Import init.qti.chg_policy.sh from LA.UM.9.1.r1-10900-SMxxx0.0"
    This reverts commit 1e498012fcf68389a9288c16e180b201c5aa668b.
    
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I58fe532ca700d2f041a680485ec0bee185a1f412

--- device.mk
+++ device.mk
@@ -237,7 +237,6 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
-    init.qti.chg_policy.sh \
     init.mi.usb.sh \
     init.recovery.qcom.rc \
     init.target.rc \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -82,14 +82,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qti.chg_policy.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/init.qti.chg_policy.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.recovery.qcom.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
