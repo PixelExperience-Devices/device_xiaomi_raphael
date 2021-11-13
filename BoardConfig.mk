a3d3d7059cef60c2727c72a383bee32782452532
Dmitrii <bankersenator@gmail.com>
2021-11-13 17:08:44 +0000

    raphael: Ignore neverallows for now
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,6 +155,7 @@ VENDOR_SECURITY_PATCH := 2021-07-01
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+SELINUX_IGNORE_NEVERALLOWS := true
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
