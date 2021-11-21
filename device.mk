d542d6be9d2082f397de1853b3a47891afd59713
Arian <arian.kulmer@web.de>
2021-11-21 15:23:38 +0100

    raphael: rootdir: Remove unnecessary early_boot script
    Change-Id: I8929b6d1686eb7a0600c99125665809d61cd2290

--- device.mk
+++ device.mk
@@ -258,7 +258,6 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.zram \
     init.insmod.sh \
-    init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \
     init.qcom.sh \
