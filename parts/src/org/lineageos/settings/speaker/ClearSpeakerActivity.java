008cb01728aa591142c09dc4ea4b70e0d09bfd9b
Joey Huab <joey@evolution-x.org>
2021-12-09 13:38:08 +0900

    raphael: parts: Adapt Clear Speaker to S style
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- parts/src/org/lineageos/settings/speaker/ClearSpeakerActivity.java
+++ parts/src/org/lineageos/settings/speaker/ClearSpeakerActivity.java
@@ -16,35 +16,20 @@
 
 package org.lineageos.settings.speaker;
 
-import android.app.Activity;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.MenuItem;
 
-public class ClearSpeakerActivity extends Activity {
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.collapsingtoolbar.R;
+
+public class ClearSpeakerActivity extends CollapsingToolbarBaseActivity {
+
+    private static final String TAG_CLEARSPEAKER = "clearspeaker";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-
-        Fragment fragment = getFragmentManager().findFragmentById(android.R.id.content);
-        ClearSpeakerFragment clearSpeakerFragment;
-        if (fragment == null) {
-            clearSpeakerFragment = new ClearSpeakerFragment();
-            getFragmentManager().beginTransaction()
-                    .add(android.R.id.content, clearSpeakerFragment)
-                    .commit();
-        }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+                new ClearSpeakerFragment(), TAG_CLEARSPEAKER).commit();
     }
 }
