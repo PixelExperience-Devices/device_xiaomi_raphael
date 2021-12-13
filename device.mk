b999fe997bb108fa0e1a80744f9840a494c10e1a
zaidkhan0997 <kzaid0997@gmail.com>
2021-12-13 19:08:41 +0200

    raphael: Build necessary camera libs

--- device.mk
+++ device.mk
@@ -121,6 +121,7 @@ PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.motor@1.0.vendor \
     libdng_sdk.vendor \
     libshim_megvii \
+    libstdc++.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
