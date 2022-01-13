71bc01a0cc9004f2444057532b14bc0dcf4a3606
Pranav Vashi <neobuddy89@gmail.com>
2022-01-13 18:34:17 +0530

    raphael: parts: Handle more error for dirac
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- parts/src/org/lineageos/settings/dirac/DiracSound.java
+++ parts/src/org/lineageos/settings/dirac/DiracSound.java
@@ -36,30 +36,35 @@ public class DiracSound extends AudioEffect {
     }
 
     public int getMusic() throws IllegalStateException,
-            IllegalArgumentException, UnsupportedOperationException {
+            IllegalArgumentException, UnsupportedOperationException,
+            RuntimeException {
         int[] value = new int[1];
         checkStatus(getParameter(DIRACSOUND_PARAM_MUSIC, value));
         return value[0];
     }
 
     public void setMusic(int enable) throws IllegalStateException,
-            IllegalArgumentException, UnsupportedOperationException {
+            IllegalArgumentException, UnsupportedOperationException,
+            RuntimeException {
         checkStatus(setParameter(DIRACSOUND_PARAM_MUSIC, enable));
     }
 
     public void setHeadsetType(int type) throws IllegalStateException,
-            IllegalArgumentException, UnsupportedOperationException {
+            IllegalArgumentException, UnsupportedOperationException,
+            RuntimeException {
         checkStatus(setParameter(DIRACSOUND_PARAM_HEADSET_TYPE, type));
     }
 
     public void setLevel(int band, float level) throws IllegalStateException,
-            IllegalArgumentException, UnsupportedOperationException {
+            IllegalArgumentException, UnsupportedOperationException,
+            RuntimeException {
         checkStatus(setParameter(new int[]{DIRACSOUND_PARAM_EQ_LEVEL, band},
                 String.valueOf(level).getBytes()));
     }
 
     public void setHifiMode(int mode) throws IllegalStateException,
-            IllegalArgumentException, UnsupportedOperationException {
+            IllegalArgumentException, UnsupportedOperationException,
+            RuntimeException {
         checkStatus(setParameter(DIRACSOUND_PARAM_HIFI, mode));
     }
 }
