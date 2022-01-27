473e3517c3ef8bd1962bc9a78d464307ad2c2efb
Joey Huab <joey@evolution-x.org>
2022-01-27 15:12:37 +0900

    raphael: bluetooth: Import bdroid_buildcfg.h
    * From https://github.com/Spark-Devices/android_device_xiaomi_raphael/blob/spark/bluetooth/include/bdroid_buildcfg.h
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -5,6 +5,7 @@
  *  for attribution purposes only.
  *
  * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2018-2019 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,10 +23,51 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
+#include <stdint.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+int property_get(const char* key, char* value, const char* default_value);
+#ifdef __cplusplus
+}
+#endif
+
+#include "osi/include/osi.h"
+
+typedef struct {
+    const char* product_region;
+    const char* product_model;
+} device_t;
+
+static const device_t devices[] = {
+        {"GLOBAL", "Mi 9T Pro"},
+        {"INDIA", "Redmi K20 Pro"},
+        {"CN", "Redmi K20 Pro"},};
+
+static inline const char* BtmGetDefaultName() {
+    char product_region[92];
+    property_get("ro.boot.hwc", product_region, "");
+
+    for (unsigned int i = 0; i < ARRAY_SIZE(devices); i++) {
+        device_t device = devices[i];
+
+        if (strcmp(device.product_region, product_region) == 0) {
+            return device.product_model;
+        }
+    }
+
+    // Fallback to ro.product.model
+    return "";
+}
+
+#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
 // Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec
 #define AVDT_NUM_SEPS 12
+
 #endif
