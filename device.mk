7a95a5ef9c97e5818bfab1740e9aecdfa672cfcd
TH779 <i@779.moe>
2022-03-01 19:22:12 +0800

    raphael: Import powerhint.json from Pixel 4 XL
     * From android-12.0.0_r32.
    
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I5f594c66c696b27b365fcee5b033e22a29c2746b

--- device.mk
+++ device.mk
@@ -354,6 +354,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/power/etc/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
