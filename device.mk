f61be33993bde934594312cd0ad4b91e29a573df
TH779 <i@779.moe>
2022-03-30 11:58:48 +0200

    raphael: overlay: Drop deprecated overlays
    Signed-off-by: TH779 <i@779.moe>
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: Ic66ffa74e6745db4c252a1fbef67d094ff7c9c90

--- device.mk
+++ device.mk
@@ -39,7 +39,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-spark
+    $(LOCAL_PATH)/overlay-voltage
 
 # ANT+
 PRODUCT_PACKAGES += \
