2504ebe374d3886e42a83bebc92fbc4060a0c2b1
Pranav Temkar <pranavtemkar@gmail.com>
2022-06-16 12:01:27 +0000

    Revert "GoogleCamera: move to sperate repo"
    Google Camera requires prebuit gapps to work and VoltageOS are not shipped with gapps so it wont work, shift to source side camera.
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    This reverts commit c8dccacb27d2b73aa6adadb13ef4b506a82dafd4.

--- device.mk
+++ device.mk
@@ -225,9 +225,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
 
-# GoogleCamera
-$(call inherit-product-if-exists, vendor/GoogleCamera/GoogleCamera.mk)
-
 # Google Photos
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/pixel/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
