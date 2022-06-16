ce9f64ba2cb10ef3d1440baab8944ab54b0a6933
Pranav Temkar <pranavtemkar@gmail.com>
2022-06-16 11:00:16 +0000

    Revert "raphael: Build power-off charging daemon dependency"
    This reverts commit bd03b6f403f9c314e9fe0de3201be80eebcf9ed5.

--- device.mk
+++ device.mk
@@ -131,10 +131,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
-# Charger
-PRODUCT_PACKAGES += \
-    libsuspend
-
 # Codec2
 PRODUCT_PACKAGES += \
     libcodec2_vndk.vendor \
