lazyd0ct0r/device_xiaomi_raphael_barny

raphael: thermal: Rebrand to Xiaomi raphael
Change-Id: Iafa72da14648a2652df9b64a02dd6e5b9fa8bd9b

thermal/service.cpp
@@ -29,7 +29,7 @@ using ::android::hardware::thermal::V2_0::IThermal;
 using ::android::hardware::thermal::V2_0::implementation::Thermal;
 
 static int shutdown() {
-    LOG(ERROR) << "Pixel Thermal HAL Service is shutting down.";
+    LOG(ERROR) << "Xiaomi Raphel Thermal HAL Service is shutting down.";
     return 1;
 }
 
@@ -37,7 +37,7 @@ int main(int /* argc */, char ** /* argv */) {
     status_t status;
     android::sp<IThermal> service = nullptr;
 
-    LOG(INFO) << "Pixel Thermal HAL Service 2.0 starting...";
+    LOG(INFO) << "Xiaomi Raphel Thermal HAL Service 2.0 starting...";
 
     service = new Thermal();
     if (service == nullptr) {
@@ -53,7 +53,7 @@ int main(int /* argc */, char ** /* argv */) {
         return shutdown();
     }
 
-    LOG(INFO) << "Pixel Thermal HAL Service 2.0 started successfully.";
+    LOG(INFO) << "Xiaomi Raphel Thermal HAL Service 2.0 started successfully.";
     joinRpcThreadpool();
     // We should not get past the joinRpcThreadpool().
     return shutdown();

PixelExperience-Devices/device_xiaomi_raphael

thermal/service.cpp
@@ -29,7 +29,7 @@ using ::android::hardware::thermal::V2_0::IThermal;
 using ::android::hardware::thermal::V2_0::implementation::Thermal;
 
 static int shutdown() {
-    LOG(ERROR) << "Pixel Thermal HAL Service is shutting down.";
+    LOG(ERROR) << "Xiaomi Raphael Thermal HAL Service is shutting down.";
     return 1;
 }
 
@@ -37,7 +37,7 @@ int main(int /* argc */, char ** /* argv */) {
     status_t status;
     android::sp<IThermal> service = nullptr;
 
-    LOG(INFO) << "Pixel Thermal HAL Service 2.0 starting...";
+    LOG(INFO) << "Xiaomi Raphael Thermal HAL Service 2.0 starting...";
 
     service = new Thermal();
     if (service == nullptr) {
@@ -53,7 +53,7 @@ int main(int /* argc */, char ** /* argv */) {
         return shutdown();
     }
 
-    LOG(INFO) << "Pixel Thermal HAL Service 2.0 started successfully.";
+    LOG(INFO) << "Xiaomi Raphael Thermal HAL Service 2.0 started successfully.";
     joinRpcThreadpool();
     // We should not get past the joinRpcThreadpool().
     return shutdown();