923322e440b5f4db707d7ae2d1c97e4fe300e940
Joey Huab <joey@evolution-x.org>
2022-08-03 02:06:15 +0000

    Revert "raphael: Switch to source built mlipay & mtdservice interface lib"
    This reverts commit f049b902ffadb8db42f6c05036fa30285485c0b4.
    This reverts commit 0957c96d161a729c9747c40a1ba9ef5e53606a50.

--- device.mk
+++ device.mk
@@ -293,11 +293,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
-# Mlipay
-PRODUCT_PACKAGES += \
-    vendor.xiaomi.hardware.mlipay@1.1.vendor \
-    vendor.xiaomi.hardware.mtdservice@1.0.vendor
-
 # Net
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
