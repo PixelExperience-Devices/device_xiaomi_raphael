c14f2da3333ce5ea7d0d439583f9e9a6798d8f4c
Joey Huab <joey@evolution-x.org>
2022-08-17 16:54:34 +0000

    [SQUASH] raphael: Revert to prebuilt BT stack
    Revert "raphael: Match QVA static overlay configs for BT"
    
    This reverts commit f9b44468ed815e4407f9df29d7abd0575c1ab0b4.
    
    Revert "raphael: Split component-overrides to support pure AOSP/QTI BT stack"
    
    This reverts commit f0091126c5e8b78de255ba0eb12830330c5982c7.
    
    Revert "raphael: Switch to QTI Bluetooth stack"
    
    This reverts commit 6e9fbc611433954732d7b3e3251ee9fd239243ca.
    
    Revert "raphael: Disable BT A2DP AAC whitelist"
    
    This reverts commit fd79fe36778c60e8edfa5a1dcf4203ac05625a1b.
    
    Revert "raphael: A2DP offload: swap audio XML files"
    
    This reverts commit 4ee4f8d9e4ae4a9ccc554c04431625bab2db9dc5.
    
    Revert "raphael: Enable BT A2DP offload"
    
    This reverts commit ed6b5b2b4ecfd57517f0ebdff8b4319bcd6184dc.
    
    Revert "raphael: Support A2DP offload for Bluetooth Audio HAL V2"
    
    This reverts commit 189e8e020c79267e2a0f6285c03c7e2821b7e24a.

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -65,8 +65,9 @@ static inline const char* BtmGetDefaultName() {
 #define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    32
+#define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
-#define GATT_MAX_PHY_CHANNEL  10
-#define AVDT_NUM_SEPS 35
+// Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec
+#define AVDT_NUM_SEPS 12
+
 #endif
--- device.mk
+++ device.mk
@@ -110,13 +110,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration_7_0.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.1.vendor \
-    libbthost_if \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth.audio@2.1-impl \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
@@ -141,8 +141,7 @@ PRODUCT_PACKAGES += \
 
 # Component overrides
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
-    $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/component-overrides.xml
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
 # Configstore
 PRODUCT_PACKAGES += \
