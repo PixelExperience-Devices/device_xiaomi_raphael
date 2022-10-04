6a97213ac25da63f0d173df1201a0c4522ca263b
Edwin Moquete <edwinmmoquete@gmail.com>
2022-10-04 22:14:39 +0000

    raphael: Switch to legacy sepolicy_vndr
    Signed-off-by: anuragbhaumik <bhaumik463@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,7 +161,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Sepolicy
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 SELINUX_IGNORE_NEVERALLOWS := true
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
