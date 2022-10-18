5df5ef1df531ea04482453cdf204336f7b464485
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-10-18 15:10:47 +0000

    raphael: Remove non-existing or implicitly included IPACM packages

--- device.mk
+++ device.mk
@@ -275,7 +275,9 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc
 
 # IPACM
-$(call inherit-product, vendor/qcom/opensource/data-ipa-cfg-mgr/ipacm_vendor_product.mk)
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml
 
 # IRSC
 PRODUCT_COPY_FILES += \
