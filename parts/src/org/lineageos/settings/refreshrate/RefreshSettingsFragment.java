5b9598fdc9b1f30721ce2fa72be23f01100d9d1d
Pranav Vashi <neobuddy89@gmail.com>
2022-11-08 18:33:19 +0530

    raphael: parts: Improve refresh rate settings layout
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- parts/src/org/lineageos/settings/refreshrate/RefreshSettingsFragment.java
+++ parts/src/org/lineageos/settings/refreshrate/RefreshSettingsFragment.java
@@ -57,8 +57,6 @@ public class RefreshSettingsFragment extends PreferenceFragment
     private ApplicationsState mApplicationsState;
     private ApplicationsState.Session mSession;
     private ActivityFilter mActivityFilter;
-    private Map<String, ApplicationsState.AppEntry> mEntryMap =
-            new HashMap<String, ApplicationsState.AppEntry>();
 
     private RefreshUtils mRefreshUtils;
     private RecyclerView mAppsRecyclerView;
@@ -182,10 +180,6 @@ public class RefreshSettingsFragment extends PreferenceFragment
         }
 
         mAllPackagesAdapter.setEntries(entries, sections, positions);
-        mEntryMap.clear();
-        for (ApplicationsState.AppEntry e : entries) {
-            mEntryMap.put(e.info.packageName, e);
-        }
     }
 
     private void rebuild() {
@@ -313,6 +307,7 @@ public class RefreshSettingsFragment extends PreferenceFragment
             holder.mode.setSelection(packageState, false);
             holder.mode.setTag(entry);
             holder.stateIcon.setImageResource(getStateDrawable(packageState));
+            holder.stateIcon.setOnClickListener(v -> holder.mode.performClick());
         }
 
         private void setEntries(List<ApplicationsState.AppEntry> entries,
