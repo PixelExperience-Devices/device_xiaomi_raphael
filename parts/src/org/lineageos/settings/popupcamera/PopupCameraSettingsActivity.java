ae81281bc2ed23a3f2b50bf4ab1a849bd25bdd00
Bruno Martins <bgcngm@gmail.com>
2022-12-07 11:03:15 +0800

    raphael: parts: Fix compilation for QPR1 merge
    Change-Id: I71ad7db85592ca18e3f0f8155bba5a472bfadbaa

--- parts/src/org/lineageos/settings/popupcamera/PopupCameraSettingsActivity.java
+++ parts/src/org/lineageos/settings/popupcamera/PopupCameraSettingsActivity.java
@@ -19,7 +19,7 @@ package org.lineageos.settings.popupcamera;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.collapsingtoolbar.R;
+import com.android.settingslib.widget.R;
 
 public class PopupCameraSettingsActivity extends CollapsingToolbarBaseActivity {
 
--- parts/src/org/lineageos/settings/refreshrate/RefreshActivity.java
+++ parts/src/org/lineageos/settings/refreshrate/RefreshActivity.java
@@ -19,7 +19,7 @@ package org.lineageos.settings.refreshrate;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.collapsingtoolbar.R;
+import com.android.settingslib.widget.R;
 
 public class RefreshActivity extends CollapsingToolbarBaseActivity {
 
