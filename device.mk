137136dd41d6be50f4287015edd8159d8c144504
xenxynon <flynryder427@gmail.com>
2023-01-18 14:01:00 +0300

    raphael: Drop mock hidl power stats
    * E android.hardware.power.stats@1.0-service-mock Failed to getEnergyData
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -388,8 +388,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/etc/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
