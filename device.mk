fb8f12c7daefa028f23d1763f97f062bed849155
Demon000 <demonsingur@gmail.com>
2023-01-23 00:07:49 +0200

    raphael: Support UDFPS on AoD

--- device.mk
+++ device.mk
@@ -449,6 +449,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libsensorndkbridge
 
+PRODUCT_PACKAGES += \
+    sensors.udfps
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Shim
 PRODUCT_PACKAGES += \
     libwatermark_shim
