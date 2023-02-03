b1386e81217936c6549d549fedf4a6939069b7fa
Debayan Kar <kardebayan3@gmail.com>
2023-02-04 02:59:43 +0530

    raphael: Fix IMS symlink as per dynamic partitions
     * We have logical /system_ext partition now, so we dont need to rely over /system/system_ext
    
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- Android.mk
+++ Android.mk
@@ -68,7 +68,7 @@ $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "IMS lib link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf /system/system_ext/lib64/$(notdir $@) $@
+	$(hide) ln -sf /system_ext/lib64/$(notdir $@) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
 
