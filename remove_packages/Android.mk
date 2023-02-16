4f4f3b6adc4f1ad0ad31a7b78d8aa0a566aa5909
Joey Huab <joey@evolution-x.org>
2023-02-16 09:50:52 +0000

    raphael: Update remove_packages

---
+++ remove_packages/Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := AdaptiveVPNPrebuilt
+LOCAL_OVERRIDES_PACKAGES += Aperture AmbientStreaming
+LOCAL_OVERRIDES_PACKAGES += CalendarGooglePrebuilt
+LOCAL_OVERRIDES_PACKAGES += CarrierLocation CarrierMetrics
+LOCAL_OVERRIDES_PACKAGES += DevicePolicyPrebuilt
+LOCAL_OVERRIDES_PACKAGES += DiagnosticsToolPrebuilt
+LOCAL_OVERRIDES_PACKAGES += FilesPrebuilt Gallery2
+LOCAL_OVERRIDES_PACKAGES += Photos PhotoTable
+LOCAL_OVERRIDES_PACKAGES += RecorderPrebuilt
+LOCAL_OVERRIDES_PACKAGES += ScribePrebuilt Showcase
+LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
