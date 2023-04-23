b6b270dcdb70475332498443f143d9bf55968edf
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-04-23 14:49:22 +0800

    raphael: Switch to common Xiaomi 1.0 sensors implementation

--- device.mk
+++ device.mk
@@ -458,7 +458,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-impl-xiaomi \
     android.hardware.sensors@1.0-service \
     android.hardware.sensors@2.0 \
     android.hardware.sensors@2.1
