c89640a9d4c45cf6423860e286c2f64c047ab574
RealJohnGalt <johngaltfirstrun@gmail.com>
2023-08-02 05:57:18 +0000

    raphael: vendor/etc/init: move two services to DT
    For task_profiles migration

--- device.mk
+++ device.mk
@@ -45,6 +45,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.incremental.enable=yes
 
+# Vendor init
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/android.hardware.drm@1.3-service.widevine.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.drm@1.3-service.widevine.rc \
+    $(LOCAL_PATH)/configs/android.hardware.neuralnetworks@1.3-service-qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.neuralnetworks@1.3-service-qti.rc
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
