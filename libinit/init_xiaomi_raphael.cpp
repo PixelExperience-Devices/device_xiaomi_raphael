0875e8f1a45577dcc7bf5067a0284795597d6494
Joey Huab <joey@evolution-x.org>
2023-08-03 19:37:42 +0900

    Revert "raphael: libinit: Workaround safetynet"
    This reverts commit eb24622e05af9b1689e7fc5069b36d0159837e85.

--- libinit/init_xiaomi_raphael.cpp
+++ libinit/init_xiaomi_raphael.cpp
@@ -5,7 +5,6 @@
  */
 
 #include <libinit_dalvik_heap.h>
-#include <libinit_utils.h>
 #include <libinit_variant.h>
 
 #include "vendor_init.h"
@@ -60,7 +59,4 @@ static const std::vector<variant_info_t> variants = {
 void vendor_load_properties() {
     set_dalvik_heap();
     search_variant(variants);
-
-    // SafetyNet workaround
-    property_override("ro.boot.verifiedbootstate", "green");
 }
