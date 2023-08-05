45ea5ffabc932353a662dfa52f8bf9334ce5bf02
Jorge <jorgelucas.sd@gmail.com>
2023-08-05 12:14:50 -0300

    Revert "raphael: parts: Introduce haptic feedback level adjustment"
    This reverts commit 83e6e66b644a43ef285da2ab8525855f8b5e694a.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,7 +23,6 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.lineageos.settings.doze.DozeUtils;
-import org.lineageos.settings.haptic.HapticUtils;
 import org.lineageos.settings.popupcamera.PopupCameraUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -35,7 +34,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
-        HapticUtils.restoreLevel(context);
         PopupCameraUtils.startService(context);
     }
 }
