4c28757fd559e00aa78fe205246f0607b3bb0aa7
Jorge <jorgelucas.sd@gmail.com>
2023-08-05 15:19:32 -0300

    Revert "raphael: Clean up duplicate dolby sepolicy entries"
    This reverts commit 13d2090b05db1a248c76c204e9c496ac918478b4.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -165,6 +165,7 @@ include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor/dolby
 BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
 
 # Verified Boot
