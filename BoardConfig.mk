9f2955fd45c3c47b8989af1669366c74651a00de
Jorge <jorgelucas.sd@gmail.com>
2023-08-04 22:24:16 -0300

    raphael: selinux: dont ignore neverallows

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,7 +161,6 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Sepolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
-SELINUX_IGNORE_NEVERALLOWS := true
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
