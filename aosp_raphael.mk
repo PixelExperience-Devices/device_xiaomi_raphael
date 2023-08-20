ae6de6fca0675f0dbe4683c35b53a94080a77bdd
Jorge <jorgelucas.sd@gmail.com>
2023-08-20 08:55:40 -0300

    raphael: Update bootanimation flag

--- aosp_raphael.mk
+++ aosp_raphael.mk
@@ -24,8 +24,7 @@ $(call inherit-product, device/xiaomi/raphael/device.mk)
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2340
-TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_raphael
