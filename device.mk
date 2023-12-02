2093096821306234a62a686f239f711249f5a20e
Jorge Lucas <jorgelucas.sd@gmail.com>
2023-12-02 23:23:44 +0000

    raphael: overlay-lineage -> overlay

--- device.mk
+++ device.mk
@@ -32,8 +32,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # ANT+
 PRODUCT_PACKAGES += \
