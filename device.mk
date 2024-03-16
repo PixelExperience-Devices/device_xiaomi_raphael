3e9b8c0380109136f9f7c9484e08a5d871da271d
Jorge Lucas <jorgelucas.sd@gmail.com>
2024-03-16 07:11:32 +0000

    raphael: Remove duplicate manifest entry's

--- device.mk
+++ device.mk
@@ -161,8 +161,6 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm@1.4-service.clearkey
 
